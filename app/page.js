7442af042d9e035e6d2c566a5b96ef95fa25a063
EdgarRamosLuna <edgarramoslun333@gmail.com>
2024-01-01 01:48:39 -0600

    Avances de sistema y landing page

--- app/page.js
+++ app/page.js
@@ -1,31 +1,20 @@
-import LandingPage from "@/components/home/LandingPage"
 import { LandingV2 } from '@/components/home/Landingv2'
 
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/schedules')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/schedules')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
  
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
  
-  return res.json()
-}
+//   return res.json()
+// }
  
 export default async function Page() {
-  const data = await getData()
+ // const data = await getData()
  
   return <LandingV2/>
 }
-// import { LandingV2 } from '@/components/home/Landingv2'
-// import React from 'react'
-
-// const page = () => {
-//   return (
-
-//   )
-// }
-
-// export default page
